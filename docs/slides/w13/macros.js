9c6ba6b9b10028901f92be8f4c06dbbf9dba59e0
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2020-12-06 16:07:06 -0500

    What I ran with on Friday

--- docs/slides/w13/macros.js
+++ docs/slides/w13/macros.js
@@ -3,4 +3,8 @@ remark.macros.scale = function(w) {
   var url = this;
   return '<img src="' + url + '" style="width: ' + w + '" />';
 };
+remark.macros.maxheight = function(h) {
+  var url = this;
+  return '<img src="' + url + '" style="max-height: ' + h + '" />';
+};
 
