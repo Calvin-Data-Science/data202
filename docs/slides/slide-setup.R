6160fb58954cd4077e004e5e261404ea0487e05e
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2021-10-25 12:29:19 -0400

    Revise setup

--- docs/slides/slide-setup.R
+++ docs/slides/slide-setup.R
@@ -1,3 +1,9 @@
+# This code should get included using a code chunk like:
+# {r setup, include=FALSE, code=xfun::read_utf8('../slide-setup.R')}
+#
+# That's important so that results are actually evaluated and printed into the Rmd.
+# xaringinextra needs this, for example.
+
 install_github_if_missing <- function(repo) {
   parts <- strsplit(repo, '/')[[1]]
   user <- parts[1]
@@ -16,6 +22,9 @@ include_graphics <- knitr::include_graphics
 kable <- knitr::kable
 glue <- glue::glue
 
+# Include styles.
+invisible(file.link("../slides.css", "slides.css"))
+invisible(file.link("../xaringan-themer.css", "xaringan-themer.css"))
 
 calvin_maroon <- "#8C2131"
 calvin_gold <- "#F3CD00"
@@ -80,46 +89,6 @@ style_mono_accent(
   outfile = "xaringan-themer.css"
 )
 
-if (FALSE) {
-  style_solarized_dark(
-    text_color = "#f6f6f6",                      # gray
-    header_color = "#A7D5E8",                    # light-blue
-    background_color = "#002b36",                # default bg color
-    link_color = "#A7D5E8",                      # light-blue
-    text_bold_color = "#E9AFA3",                 # pink
-    code_highlight_color = "#A7D5E8",            # light-blue
-    code_inline_color = "#A7A7A7",               # gray
-    inverse_background_color = "#ffffff",        # white
-    inverse_text_color = "#002b36",              # default bg color
-    inverse_header_color = "#A7D5E8",            # light-blue
-    title_slide_text_color = "#A7D5E8",          # light-blue
-    title_slide_background_color = "#002b36",    # default bg color
-    left_column_subtle_color = "#586e75",        # some gray
-    left_column_selected_color = "#93a1a1",      # some lighter gray
-    blockquote_left_border_color = "#e9d968",    # yellow
-    table_border_color = "#8A8A8A",              # gray
-    table_row_border_color = "#8A8A8A",          # gray
-    table_row_even_background_color = "#004a5d", # slightly lighter dark-blue
-    base_font_size = "28px",
-    header_h1_font_size = "2rem",
-    header_h2_font_size = "1.75rem",
-    header_h3_font_size = "1.5rem",
-    text_font_google = google_font("Source Sans Pro"),
-    text_font_family = xaringanthemer_font_default("text_font_family"),
-    text_font_weight = xaringanthemer_font_default("text_font_weight"),
-    text_font_url = xaringanthemer_font_default("text_font_url"),
-    text_font_family_fallback = xaringanthemer_font_default("text_font_family_fallback"),
-    text_font_base = "sans-serif",
-    code_font_google = google_font("Source Code Pro"),
-    code_font_family = xaringanthemer_font_default("code_font_family"),
-    code_font_size = "20px",
-    code_font_url = xaringanthemer_font_default("code_font_url"),
-    code_font_family_fallback = xaringanthemer_font_default("code_font_family_fallback"),
-    outfile = "../xaringan-themer.css"
-  )
-}
-#xaringanExtra::use_xaringan_extra(c(
-#  "tile_view", "animate_css", "editable", "tachyons", "panelset", "clipboard"))
 library(xaringanExtra)
 xaringanExtra::use_panelset(in_xaringan = TRUE)
 xaringanExtra::use_tile_view()
